5752f539aa2aba221822b412439a02e0ee98e161
Doug Swisher <big.swish@gmail.com>
2018-06-17 10:54:43 -0500

    chap2 - catanimation

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This repo contains my work going through the online book [Making Games with Python and Pygame](http://inventwithpython.com/pygame/).
 There is a PDF version [here](https://inventwithpython.com/makinggames.pdf), along with sources and other files [here](http://inventwithpython.com/makinggames.zip), and [errata](http://inventwithpython.com/pygame/errata).
 
+For some of the scripts to work, you need images and whatnot from the author's archive. Unzip those into the `SOURCE` directory.
+
 ### Other Misc Links
 
 * [Pygame](https://www.pygame.org/) and [docs](https://www.pygame.org/docs/)
---
+++ chap2/catanimation.py
@@ -0,0 +1,49 @@
+import os
+import pygame
+import sys
+
+pygame.init()
+
+FPS = 30    # frames per second setting
+fpsClock = pygame.time.Clock()
+
+# set up the window
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption('Animation')
+
+WHITE = (255, 255, 255)
+catPath = os.path.abspath(os.path.join(os.getcwd(), os.pardir, 'SOURCE', 'cat.png'))
+catImg = pygame.image.load(catPath)
+catx = 10
+caty = 10
+direction = 'right'
+
+while True:  # the main game loop
+    DISPLAYSURF.fill(WHITE)
+
+    if direction == 'right':
+        catx += 5
+        if catx == 280:
+            direction = 'down'
+    elif direction == 'down':
+        caty += 5
+        if caty == 220:
+            direction = 'left'
+    elif direction == 'left':
+        catx -= 5
+        if catx == 10:
+            direction = 'up'
+    elif direction == 'up':
+        caty -= 5
+        if caty == 10:
+            direction = 'right'
+
+    DISPLAYSURF.blit(catImg, (catx, caty))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
